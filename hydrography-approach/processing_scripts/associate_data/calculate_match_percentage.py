bad32ba922fb1345b7f869e21ea540d36d1e2768
Pushpak Gote <pushpakg@mapup.ai>
2024-08-06 22:04:10 +0530

    Added scripts for matching fuzzy similarity score from new OSM columns and state neighboring roads

--- hydrography-approach/processing_scripts/associate_data/calculate_match_percentage.py
+++ hydrography-approach/processing_scripts/associate_data/calculate_match_percentage.py
@@ -1,34 +1,93 @@
 import pandas as pd
+from typing import List, Tuple, Optional
 from fuzzywuzzy import fuzz
 
-
 # Function to calculate similarity
-def calculate_osm_similarity(row):
-    return fuzz.token_sort_ratio(
-        row["osm_name"], row["7 - Facility Carried By Structure"]
-    )
+def calculate_similarity(row: pd.Series, cols: List[str], fixed_column: str) -> Tuple[int, Optional[str]]:
+    """
+    Calculate the maximum similarity score between a row and a list of columns with respect to a fixed column.
 
+    Parameters:
+        row (pandas.Series): A row from a DataFrame.
+        cols (list): A list of column names to compare.
+        fixed_column (str): The column name to compare against.
 
-def calculate_nhd_similarity(row):
-    return fuzz.token_sort_ratio(row["stream_name"], row["6A - Features Intersected"])
+    Returns:
+        tuple: A tuple containing the maximum similarity score and the column name with the maximum similarity score.
+    """
+    try:
+        max_score = 0
+        max_col = None
+        for col in cols:
+            if col in row and pd.notna(row[col]) and pd.notna(row[fixed_column]):
+                score = fuzz.token_sort_ratio(str(row[col]), str(row[fixed_column]))
+                if score > max_score:
+                    max_score = score
+                    max_col = col
+        return max_score, max_col
+    except Exception as e:
+        print(f"Error occurred while calculating similarity: {e}")
+        raise Exception(f"Error occurred while calculating similarity: {e}")
 
+def read_exploded_osm_data_csv(exploded_osm_data_csv: str, osm_cols_for_road_names: List[str]) -> Tuple[pd.DataFrame, List[str]]:
+    """
+    Reads the 'exploded_osm_data' CSV file and returns a DataFrame with the required columns.
 
-def calculate_cross_similarity_1(row):
-    return fuzz.token_sort_ratio(row["osm_name"], row["6A - Features Intersected"])
+    Parameters:
+        exploded_osm_data_csv (str): The path to the CSV file.
+        osm_cols_for_road_names (list): The list of column names to read from the CSV file.
 
+    Returns:
+        pandas.DataFrame: The DataFrame containing the required columns.
+    """
+    series_list=[]
+    available_osm_road_names=[]
+    for col in osm_cols_for_road_names:
+        try:
+            series=pd.read_csv(exploded_osm_data_csv,usecols=[col])
+            series_list.append(series)
+            available_osm_road_names.append(col)
+        except Exception as e:
+            print(e)
+            continue
+    
+    try:
+        exploded_osm_data_df=pd.concat(series_list,axis=1)
+        return exploded_osm_data_df, available_osm_road_names
+    except Exception as e:
+        print(f"Error occurred while concatenating the series: {e}")
+        raise Exception(f"Error occurred while concatenating the series: {e}")
+    
 
-def calculate_cross_similarity_2(row):
-    return fuzz.token_sort_ratio(
-        row["stream_name"], row["7 - Facility Carried By Structure"]
-    )
+def run(bridge_with_proj_points, bridge_match_percentage,exploded_osm_data_csv):
 
-
-def run(bridge_with_proj_points, bridge_match_percentage):
     df = pd.read_csv(bridge_with_proj_points)
 
-    # Apply the function row-wise
-    df["osm_similarity"] = df.apply(calculate_osm_similarity, axis=1)
-    df["nhd_similarity"] = df.apply(calculate_nhd_similarity, axis=1)
+    # Read the 'exploded_osm_data' CSV file and select the required columns
+    osm_cols_for_road_names=["osm_id",  "name",  "ref",    "name_1",    "name_2",    "name_3",    "name_5",    "name_4",
+                        "name1",    "tiger:name_base_1",    "tiger:name_base_2",    "tiger:name_base_3",
+                        "tiger:name_base",    "alt_name",    "name:en",    "official_name",    "bridge:name"]
+
+    # Read only required columns one at a time because the CSV file is too large to read all at once
+    exploded_osm_data_df,available_osm_road_names = read_exploded_osm_data_csv(exploded_osm_data_csv, osm_cols_for_road_names)
+
+    df['final_osm_id'] = df['final_osm_id'].astype('object')
+    exploded_osm_data_df['osm_id'] = exploded_osm_data_df['osm_id'].astype('object')
+
+    #Merge the data on 'final_osm_id' and 'osm_id'
+    df = pd.merge(df, exploded_osm_data_df, left_on='final_osm_id', right_on='osm_id', how='left')
+
+    available_osm_road_names.remove('osm_id')
+
+    #get osm_similarity
+    fixed_column_1="7 - Facility Carried By Structure"
+    fixed_column_2="6A - Features Intersected"
+    df["osm_similarity"], df["osm_similarity_col"] = zip(*df.apply(lambda x: max([calculate_similarity(x, available_osm_road_names, fixed_column_1), calculate_similarity(x, available_osm_road_names, fixed_column_2)], key=lambda item: item[0]), axis=1))
+
+    #get nhd_similarity
+    nhd_cols_for_stream_name=["stream_name"]
+    fixed_column="6A - Features Intersected"
+    df["nhd_similarity"], df["nhd_similarity_col"] = zip(*df.apply(lambda x: calculate_similarity(x, nhd_cols_for_stream_name, fixed_column), axis=1))
 
     # Save the DataFrame with similarity scores
     df.to_csv(bridge_match_percentage, index=False)
--- merge-approaches/get_merged_association_output.py
+++ merge-approaches/get_merged_association_output.py
@@ -1,256 +1,178 @@
 import geopandas as gpd
-import numpy as np
 import pandas as pd
 from fuzzywuzzy import fuzz
+from typing import Union, Tuple, List
 
 
 # Function to calculate similarity
-def calculate_osm_similarity(row):
-    return fuzz.token_sort_ratio(row["name"], row["7 - Facility Carried By Structure"])
+def calculate_osm_similarity(name: str, target: str) -> float:
+    """
+    Calculate the similarity between two strings using the token_sort_ratio function from the fuzz library.
+
+    Parameters:
+        name (str): The first string to compare.
+        target (str): The second string to compare.
+
+    Returns:
+        int: The similarity score between the two strings, ranging from 0 to 100.
+    """
+    try:
+        return fuzz.token_sort_ratio(name, target)
+    except Exception as e:
+        print(f"Error occurred while calculating OSM similarity: {e}")
+        raise Exception(f"Error occurred while calculating OSM similarity: {e}")
 
 
-def read_geopackage_to_dataframe(filepath):
-    """Read a GeoPackage file into a GeoDataFrame."""
-    return gpd.read_file(filepath)
+def read_geopackage_to_dataframe(filepath: str) -> gpd.GeoDataFrame:
+    """
+    Read a GeoPackage file into a GeoDataFrame.
+
+    Args:
+        filepath (str): Path to the GeoPackage file.
+
+    Returns:
+        gpd.GeoDataFrame: The read GeoDataFrame.
+    """
+    try:
+        return gpd.read_file(filepath)
+    except Exception as e:
+        print(f"Error occurred while reading GeoPackage file: {e}")
+        raise Exception(f"Error occurred while reading GeoPackage file: {e}")
 
 
-def extract_coordinates(geom):
+def extract_coordinates(geom: object) -> Union[Tuple[float, float] , Tuple[None, None]]:
+    """
+    Extract the coordinates from a geometry object.
+
+    Args:
+        geom (object): The geometry object from which to extract the coordinates.
+
+    Returns:
+        tuple[float, float] | tuple[None, None]: A tuple containing the x and y coordinates of the geometry object,
+        or None if the geometry object is None or NaN.
+    """
     # Function to extract coordinates from geometry object
-    if geom is None or pd.isnull(geom):
-        return None, None
-    else:
-        return geom.x, geom.y
+    try:
+        if geom is None or pd.isnull(geom):
+            return None, None
+        else:
+            return geom.x, geom.y
+    except Exception as e:
+        print(f"Error occurred while extracting coordinates: {e}")
+        raise Exception(f"Error occurred while extracting coordinates: {e}")
+
+def calculate_similarity_for_neighbouring_roads(
+    merge_df: pd.DataFrame,
+    neighbouring_roads_col: str,
+    fixed_cols: List[str],
+    column_name_to_store_similarity: str
+) -> pd.DataFrame:
+    """
+    Calculate the similarity between the fixed columns and the neighbouring roads for each row in the merge_df DataFrame.
+    
+    Parameters:
+        merge_df (pandas.DataFrame): The DataFrame containing the merged data.
+        neighbouring_roads_col (str): The name of the column in merge_df that contains the neighbouring roads.
+        fixed_cols (List[str]): The list of fixed columns to compare with the neighbouring roads.
+        neighbouring_roads_similarity_col_name (str): The name of the column to store the similarity score.
+    
+    Returns:
+        pandas.DataFrame: The merge_df DataFrame with the added similarity columns and the dropped similarity columns.
+    """
+    try:
+        neighbouring_roads_expanded_df=merge_df[neighbouring_roads_col].str.split(',', expand=True)
+        neighbouring_roads_expanded_df=pd.concat([neighbouring_roads_expanded_df, merge_df[fixed_cols]], axis=1)
+        
+        merge_df[fixed_cols[0]+'_similarity']=neighbouring_roads_expanded_df.apply(lambda x: max([ calculate_osm_similarity(x[col], x[fixed_cols[0]]) for col in neighbouring_roads_expanded_df.columns if col not in fixed_cols]), axis=1)
+        merge_df[fixed_cols[1]+'_similarity']=neighbouring_roads_expanded_df.apply(lambda x: max([ calculate_osm_similarity(x[col], x[fixed_cols[1]]) for col in neighbouring_roads_expanded_df.columns if col not in fixed_cols]), axis=1)
+        merge_df[column_name_to_store_similarity]=merge_df[[fixed_cols[0]+'_similarity',fixed_cols[1]+'_similarity']].max(axis=1)
+        merge_df.drop([fixed_cols[0]+'_similarity',fixed_cols[1]+'_similarity'], axis=1, inplace=True)
+        
+        return merge_df
+    except Exception as e:
+        print(f"Error occurred while calculating similarity for neighbouring roads: {e}")
+        raise Exception(f"Error occurred while calculating similarity for neighbouring roads: {e}")
 
 
 def main():
-    mile_point_output = "mile-point-approach/output-data/osm_road_points.gpkg"
-    hydrography_output = "hydrography-approach/output-data/Kentucky/csv-files/Final-bridges-with-percentage-match.csv"
-    similarity_threshold = 75
-    review_threshold = 60
+    try:
+        neighbouring_roads_output = "grouped_neighbouring_roads.csv"
+        mile_point_output = "osm_road_points.gpkg"
+        hydrography_output = "hydrography-method/output-data/csv-files/Final-bridges-with-percentage-match.csv"
+        similarity_threshold = 70
 
-    # Read GeoPackage and CSV into DataFrames
-    milepoint_df = read_geopackage_to_dataframe(mile_point_output)
-    milepoint_df = milepoint_df.to_crs("EPSG:4326")
-    hydrography_df = pd.read_csv(hydrography_output)
+        # Read GeoPackage and CSV into DataFrames
+        milepoint_df = read_geopackage_to_dataframe(mile_point_output)
+        milepoint_df = milepoint_df.to_crs("EPSG:4326")
+        hydrography_df = pd.read_csv(hydrography_output,dtype={"final_osm_id":object})
+        neighbouring_roads_df = pd.read_csv(neighbouring_roads_output,dtype={"osm_id":object})
 
-    # Merge DataFrames and select desired columns
-    milepoint_cols = ["bridge_id", "osm_id", "name", "geometry"]
-    merge_df = pd.merge(
-        hydrography_df,
-        milepoint_df[milepoint_cols],
-        left_on="8 - Structure Number",
-        right_on="bridge_id",
-        how="left",
-    )
-    merge_df.rename(columns={"osm_similarity": "osm_similarity_hydro"}, inplace=True)
-    merge_df.rename(columns={"final_osm_id": "osm_id_hydro"}, inplace=True)
-    merge_df.rename(columns={"osm_id": "osm_id_mile"}, inplace=True)
+        # Remove the trailing '.0' from the specified column
+        hydrography_df['final_osm_id'] = hydrography_df['final_osm_id'].apply(lambda x: str(x).replace('.0', '') if isinstance(x, str) and x.endswith('.0') else x)
+        
 
-    merge_df["osm_similarity_mile"] = merge_df.apply(calculate_osm_similarity, axis=1)
+        # Merge DataFrames and select desired columns
+        milepoint_df.rename(columns={"osm_id": "osm_id_mile"}, inplace=True)
+        milepoint_cols = ["bridge_id", "osm_id_mile", "name", "geometry"]
 
-    merge_df["projected_long_mile"], merge_df["projected_lat_mile"] = zip(
-        *merge_df["geometry"].apply(extract_coordinates)
-    )
-
-    # Removing rows where coordinates from milepoint approach are not available
-    merge_df = merge_df.dropna(
-        subset=[
-            "projected_long_mile",
-            "projected_lat_mile",
-        ]
-    )
-
-    # Condition 1
-    condition1 = merge_df[
-        merge_df["bridge_id"].isnull()
-        & merge_df["osm_id_hydro"].notnull()
-        & (merge_df["osm_similarity_hydro"] >= similarity_threshold)
-    ].copy()
-    condition1["final_osm_id"] = condition1["osm_id_hydro"]
-    condition1["osm_match"] = condition1["osm_similarity_hydro"]
-    condition1["final_long"] = condition1["projected_long"]
-    condition1["final_lat"] = condition1["projected_lat"]
-
-    # Condition 1R: Review
-    condition1R = merge_df[
-        merge_df["bridge_id"].isnull()
-        & merge_df["osm_id_hydro"].notnull()
-        & (
-            (similarity_threshold > merge_df["osm_similarity_hydro"])
-            & (merge_df["osm_similarity_hydro"] >= review_threshold)
+        merge_df = pd.merge(
+            hydrography_df,
+            milepoint_df[milepoint_cols],
+            left_on="8 - Structure Number",
+            right_on="bridge_id",
+            how="left",
         )
-    ].copy()
-    condition1R["final_osm_id"] = condition1R["osm_id_hydro"]
-    condition1R["osm_match"] = condition1R["osm_similarity_hydro"]
-    condition1R["final_long"] = condition1R["projected_long"]
-    condition1R["final_lat"] = condition1R["projected_lat"]
+        merge_df.rename(columns={"osm_similarity": "osm_similarity_hydro"}, inplace=True)
+        merge_df.rename(columns={"final_osm_id": "osm_id_hydro"}, inplace=True)
+        # merge_df.rename(columns={"osm_id": "osm_id_mile"}, inplace=True)
 
-    # Condition 2
-    condition2 = merge_df[
-        merge_df["bridge_id"].notnull()
-        & (merge_df["osm_id_hydro"] == merge_df["osm_id_mile"])
-        & (merge_df["osm_similarity_hydro"] >= similarity_threshold)
-    ].copy()
-    condition2["final_osm_id"] = condition2["osm_id_hydro"]
-    condition2["osm_match"] = condition2["osm_similarity_hydro"]
-    condition2["final_long"] = condition2["projected_long_mile"]
-    condition2["final_lat"] = condition2["projected_lat_mile"]
-
-    # Condition 2R: Review
-    condition2R = merge_df[
-        merge_df["bridge_id"].notnull()
-        & (merge_df["osm_id_hydro"] == merge_df["osm_id_mile"])
-        & (
-            (similarity_threshold > merge_df["osm_similarity_hydro"])
-            & (merge_df["osm_similarity_hydro"] >= review_threshold)
+        # Merge on neighbouring roads
+        merge_df = merge_df.merge(
+            neighbouring_roads_df,
+            left_on=["osm_id_mile","bridge_id"],
+            right_on=["osm_id","bridge_id"],
+            how="left",
         )
-    ].copy()
-    condition2R["final_osm_id"] = condition2R["osm_id_hydro"]
-    condition2R["osm_match"] = condition2R["osm_similarity_hydro"]
-    condition2R["final_long"] = condition2R["projected_long_mile"]
-    condition2R["final_lat"] = condition2R["projected_lat_mile"]
 
-    # Condition 3
-    condition3 = merge_df[
-        (merge_df["bridge_id"].notnull())
-        & (merge_df["osm_id_hydro"] != merge_df["osm_id_mile"])
-        & (merge_df["osm_similarity_hydro"] > merge_df["osm_similarity_mile"])
-        & (merge_df["osm_similarity_hydro"] >= similarity_threshold)
-    ].copy()
-    condition3["final_osm_id"] = condition3["osm_id_hydro"]
-    condition3["osm_match"] = condition3["osm_similarity_hydro"]
-    condition3["final_long"] = condition3["projected_long"]
-    condition3["final_lat"] = condition3["projected_lat"]
+        #For final stats, to see blanks in point geometry
+        #merge_df.to_csv("unsnapped.csv",index=False)
 
-    # Condition 3R: Review
-    condition3R = merge_df[
-        (merge_df["bridge_id"].notnull())
-        & (merge_df["osm_id_hydro"] != merge_df["osm_id_mile"])
-        & (merge_df["osm_similarity_hydro"] > merge_df["osm_similarity_mile"])
-        & (
-            (similarity_threshold > merge_df["osm_similarity_hydro"])
-            & (merge_df["osm_similarity_hydro"] >= review_threshold)
+        # Calculate similarity for neighbouring roads
+        neighbouring_roads_col='neighbouring_roads'
+        fixed_cols=['7 - Facility Carried By Structure','6A - Features Intersected']
+        column_name_to_store_similarity="neighbouring_roads_similarity"
+        merge_df=calculate_similarity_for_neighbouring_roads(merge_df, neighbouring_roads_col, fixed_cols,column_name_to_store_similarity)
+
+        #Get max similarity between osm road names and neighbouring roads names
+        merge_df['combined_max_similarity']=merge_df[['neighbouring_roads_similarity','osm_similarity_hydro']].max(axis=1)
+        merge_df['combined_max_similarity_col']=merge_df['osm_similarity_col']
+        merge_df.loc[merge_df['osm_similarity_hydro'] < merge_df['neighbouring_roads_similarity'],['combined_max_similarity_col']]="neighbouring_roads"
+
+        # Extract coordinates from geometry
+        merge_df["projected_long_mile"], merge_df["projected_lat_mile"] = zip(
+            *merge_df["geometry"].apply(extract_coordinates)
         )
-    ].copy()
-    condition3R["final_osm_id"] = condition3R["osm_id_hydro"]
-    condition3R["osm_match"] = condition3R["osm_similarity_hydro"]
-    condition3R["final_long"] = condition3R["projected_long"]
-    condition3R["final_lat"] = condition3R["projected_lat"]
 
-    # Condition 4
-    condition4 = merge_df[
-        (merge_df["bridge_id"].notnull())
-        & (merge_df["osm_id_hydro"] != merge_df["osm_id_mile"])
-        & (merge_df["osm_similarity_hydro"] < merge_df["osm_similarity_mile"])
-        & (merge_df["osm_similarity_mile"] >= similarity_threshold)
-    ].copy()
-    condition4["final_osm_id"] = condition4["osm_id_mile"]
-    condition4["osm_match"] = condition4["osm_similarity_mile"]
-    condition4["final_long"] = condition4["projected_long_mile"]
-    condition4["final_lat"] = condition4["projected_lat_mile"]
+        #Save data where hydro osm id and milepoint osm id are same
+        merge_df=merge_df[merge_df['osm_id_mile']==merge_df['osm_id_hydro']]
 
-    # Condition 4R: Review
-    condition4R = merge_df[
-        (merge_df["bridge_id"].notnull())
-        & (merge_df["osm_id_hydro"] != merge_df["osm_id_mile"])
-        & (merge_df["osm_similarity_hydro"] < merge_df["osm_similarity_mile"])
-        & (
-            (similarity_threshold > merge_df["osm_similarity_mile"])
-            & (merge_df["osm_similarity_mile"] >= review_threshold)
-        )
-    ].copy()
-    condition4R["final_osm_id"] = condition4R["osm_id_mile"]
-    condition4R["osm_match"] = condition4R["osm_similarity_mile"]
-    condition4R["final_long"] = condition4R["projected_long_mile"]
-    condition4R["final_lat"] = condition4R["projected_lat_mile"]
+        #removing null geometry
+        merge_df=merge_df[~merge_df['geometry'].isnull()]
 
-    # Condition 5
-    condition5 = merge_df[
-        (merge_df["bridge_id"].notnull())
-        & (merge_df["osm_id_hydro"] != merge_df["osm_id_mile"])
-        & (merge_df["osm_similarity_hydro"] == merge_df["osm_similarity_mile"])
-        & (merge_df["osm_similarity_mile"] >= similarity_threshold)
-    ].copy()
-    condition5["final_osm_id"] = condition5["osm_id_mile"]
-    condition5["osm_match"] = condition5["osm_similarity_mile"]
-    condition5["final_long"] = condition5["projected_long_mile"]
-    condition5["final_lat"] = condition5["projected_lat_mile"]
+        #Automated and Maproulette edits 
+        merge_df['osm_edits']="Automated"
+        merge_df.loc[(merge_df["Unique_Bridge_OSM_Combinations"] > 1 ) & ( merge_df["combined_max_similarity"]<similarity_threshold),"osm_edits"]="Maproulette"
 
-    # Condition 5R: Review
-    condition5R = merge_df[
-        (merge_df["bridge_id"].notnull())
-        & (merge_df["osm_id_hydro"] != merge_df["osm_id_mile"])
-        & (merge_df["osm_similarity_hydro"] == merge_df["osm_similarity_mile"])
-        & (
-            (similarity_threshold > merge_df["osm_similarity_mile"])
-            & (merge_df["osm_similarity_mile"] >= review_threshold)
-        )
-    ].copy()
-    condition5R["final_osm_id"] = condition5R["osm_id_mile"]
-    condition5R["osm_match"] = condition5R["osm_similarity_mile"]
-    condition5R["final_long"] = condition5R["projected_long_mile"]
-    condition5R["final_lat"] = condition5R["projected_lat_mile"]
+        # Select desired columns for output
+        keep_cols = ['8 - Structure Number', 'osm_id_hydro', 'osm_name', 'final_stream_id', 'stream_name', '6A - Features Intersected', '7 - Facility Carried By Structure', 'bridge_length', 'Unique_Bridge_OSM_Combinations', 'combined_max_similarity', 'projected_long_mile', 'projected_lat_mile', 'osm_edits']
+        merge_df=merge_df[keep_cols]
+        merge_df.rename(columns={"osm_id_hydro": "osm_id"}, inplace=True)
 
-    # Combine all subsets
-    filtered_df = pd.concat(
-        [condition1, condition2, condition3, condition4, condition5]
-    )
-
-    filtered_review_df = pd.concat(
-        [condition1R, condition2R, condition3R, condition4R, condition5R]
-    )
-
-    # Remove duplicates
-    filtered_df = filtered_df.drop_duplicates()
-    filtered_review_df = filtered_review_df.drop_duplicates()
-
-    filtered_df = filtered_df[
-        [
-            "8 - Structure Number",
-            "final_osm_id",
-            "bridge_length",
-            "osm_match",
-            "final_lat",
-            "final_long",
-        ]
-    ]
-
-    filtered_review_df = filtered_review_df[
-        [
-            "8 - Structure Number",
-            "final_osm_id",
-            "bridge_length",
-            "osm_match",
-            "final_lat",
-            "final_long",
-        ]
-    ]
-
-    # Filter out rows where projection point is not available
-    filtered_df = filtered_df.dropna(
-        subset=[
-            "final_long",
-            "final_lat",
-        ]
-    )
-
-    filtered_review_df = filtered_review_df.dropna(
-        subset=[
-            "final_long",
-            "final_lat",
-        ]
-    )
-
-    # Save the filtered DataFrame to CSV
-    combined_df = pd.concat([filtered_df, filtered_review_df])
-    combined_df["Association"] = np.where(
-        combined_df.index.isin(filtered_df.index), "Automated import", "MapRoulette"
-    )
-    combined_df.to_csv(
-        "merge-approaches/output-data/merged-approaches-association-output.csv",
-        index=False,
-    )
-
+        merge_df.to_csv("merged-approaches-association-output.csv", index=False)
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise Exception(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
